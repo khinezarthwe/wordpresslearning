8dfc592b574a8d8bd692f4f2f55caf578315c809
administrator <administrator@015-dll-mmMacBook-Air.local>
2017-09-20 15:00:04 +0630

    Download file done

--- wp-content/plugins/kz_option/kz_option.php
+++ wp-content/plugins/kz_option/kz_option.php
@@ -1,39 +1,58 @@
 <?php 
 error_reporting(E_All);
-define( 'MY_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
-
 /*
 Plugin Name:  KZoption 
 Plugin URI:   https://github.com/khinezarthwe
-Description:  Collecting CSV file for the the user that admin want to collect these user action. Save all the information of specific user to .csv file.
+Description:  Collecting CSV file for the user that admin want to collect these user action. Save all the information of specific user to .csv file.
 
 Date:	      20170919
 Version:      1.0
 Author:       Khine Zar Thwe
 Author URI:   https://github.com/khinezarthwe
 */
-
-/**============================================================================================
+/*  
+/**======================================================================================
 * Class for custom functions. <kzt>
-*==============================================================================================
+*======================================================================================
 */
+// function kzoption_style_script() {
+// 	wp_enqueue_style( 'kzOptionStyle', plugin_dir_url( __FILE__ ) . '/css/kz_optioncss.css' );
+// }
+// add_action( 'wp_enqueue_scripts', 'kzoption_style_script' );
+if ( !class_exists( 'KZ_Menu' ) ) {
 class KZ_Menu 
 {
-	
+
 public function __construct()
 	{
-		
+		$this->kzcustom_menu();
 	}
+
+/**============================================================================================
+* Custom Menu with add_menu_page action
+ parameters are (operation page, name can be seen in admin menu, who can access, path or unique name for weblink, function, icon or logo , position)
+*==============================================================================================
+*/
 
 public function kzcustom_menu() // add the admin menu
 	{ 
-		add_menu_page('kz_option.php', 'kz_menu', 'administrator', __FILE__ ,array('KZ_Menu',
-			'display_option_page'),'dashicons-products');
-		// parameters are listed as below
-		//operation page, name can be seen in admin menu, who can access, path or unique name for weblink, function, icon or logo 
+
+		add_menu_page(
+			'kz_option.php',
+			'kz_menu',
+			'administrator',
+			__FILE__ ,
+			array('KZ_Menu',
+				  'display_option_page'),
+			'dashicons-products',25);
+		
 	}
 	
-	// function for custopm menu display 
+/**============================================================================================
+* Adding Admin Menu display function 
+checking user permission
+*==============================================================================================
+*/
 public function display_option_page()
 	{
 		// General check for user permissions.
@@ -45,71 +64,95 @@ public function display_option_page()
 		<div class="container">  
 			<h2> <span class="dashicons dashicons-thumbs-up"></span>Hello from Khine Zar Custom Menu Page </h2>
 			<h3>You can download the csv file of user custom post.</h3>
-				<form action="" method="POST">
+			<?php KZ_Menu::download_fileview(); ?>
+		</div>
+
+	<?php  
+	}
+/**============================================================================================
+* Admin .csv file download file via UI
+* creating form for submitting
+*==============================================================================================
+*/
+public function download_fileview()
+	{ ?>
+<form action="" method="POST">
 				   <?php 
 				   	$users = get_users( $args );
 				   		if ($users) 
 				   		{ ?>
-    						<select name="user_dropdown">
+
+    						<h3>Select the user that you want to save his or her record with csv file <select name="user_dropdown">
 	        					<?php foreach ($users as $user) {
 	           						echo '<option value="' .$user->ID .'">' .$user->display_name .'</option>';
 	       					 	} ?>
-    						</select>
+    						</select></h3>
     						<?php $user_id = $_POST['user_dropdown'];	
 						}
 				   ?>
-				   <input type="submit" name="submit"  class="button button-primary button-large" value="Download the Data" />
-				</form>
-		</div>
+				   
 
-		<?php // when the form submitted!
-			if(isset($_POST['submit']))
-				{
-					KZ_Menu::kz_csv_export($_POST['user_dropdown']); //call the csv_export function by giving user parameter action		   
-				} 
+				    <!-- <input type="checkbox" name="downloadoption[]" id = "downloadoption" value="comments"> Numbers Comments<br>
+  					<input type="checkbox" name="downloadoption[]" id = "downloadoption" value="Post"> Post Conent<br>
+  					<input type="checkbox" name="downloadoption[]" id = "downloadoption"value="Posttitle" > Post Title<br>
+  					<br> -->
+				   <input type="submit" name="submit"  class="button button-primary button-large" value="Download the Data" />
+
+				</form>
+			<?php
 	}
 
-public function kz_csv_export($user_id) 
-	{ //export function
 
-		$filename = 'customlist' . time() . '.csv';
+
+/**============================================================================================
+* CSV export function via submit click 
+*==============================================================================================
+*/
+public function kz_csv_export($user_id) 
+	{ 	//export function
+		die();
+		$filename = 'user_' . $user_id . 'data.csv';
 		$header_row = array(
 					0 => 'Post Title',
 					1 => 'Post Content',
+					2 => 'Total Comment on this Post',
 				);
 
 		$data_rows = array();
 		global $wpdb;
-		$post_by_user = $wpdb->get_results( "SELECT post_title,post_content FROM {$wpdb->posts} WHERE post_author={$user_id}");
-		#post_by_user = $wpdb->get_results( "SELECT posts->post_title,posts->post_content, FROM {$wpdb->posts} WHERE post_author={$user_id}");
- 		foreach ( $post_by_user as $u  ) {
+		$post_by_user = $wpdb->get_results( "SELECT post_title,post_content,comment_count FROM {$wpdb->posts} WHERE post_author={$user_id}");
+
+		if(!empty($post_by_user)){
+ 				foreach ( $post_by_user as $u  ) {
 					$row = array();
-					$row[0] = $u->post_title;
-					$row[1] = $u->post_content;
+					$row[0] = strip_tags($u->post_title);
+					$row[1] = strip_tags($u->post_content);
+					$row[2] = strip_tags($u->comment_count);
 					$data_rows[] = $row;
 				}
 
-		header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-        header('Content-Description: File Transfer');
-        header('Content-Type:application/csv;charset=UTF-8');
-        header('Content-Disposition: attachment; filename=' . 
-        $filename);
-        header('Expires: 0');
-        header('Pragma: public');
-		$fh = fopen('php://output','w');
+		header('Content-Type: text/csv');
+        header('Content-Disposition: attachment; filename=' . $filename);
+		$fh = fopen('php://output','wb');
 		fputcsv($fh, $header_row);
 		foreach ( $data_rows as $data_row )
 			{
 			 	fputcsv($fh,$data_row);
+
 			}
 		fclose( $fh );
-		#die();
-
+		die();
+		}
+		else{
+			echo '<script language="javascript">';
+			echo 'alert("This user do not have any record in this blog!")';
+			echo '</script>';
+			
+		}
 	}
 
 } 
-
-
+}
 
 //end of class
 
@@ -119,7 +162,17 @@ public function kz_csv_export($user_id)
 */
 
 add_action('admin_menu', function(){
-	KZ_Menu::kzcustom_menu();
+	new KZ_Menu();
+	#KZ_Menu::kzcustom_menu();
 });
 
+/**============================================================================================
+* Checking Post Submit and download the csv file
+*==============================================================================================
+*/
+
+if(isset($_POST['submit']))
+	{
+		KZ_Menu::kz_csv_export($_POST['user_dropdown']);//call the csv_export function by giving user parameter action		   
+	} 
 ?>
